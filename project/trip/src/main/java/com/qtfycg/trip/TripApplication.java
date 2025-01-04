f7da3c35115bf5f50c82a26a97fed51df8d874e6
qtfycg <1264318507@qq.com>
2025-01-04 16:16:33 +0800

    1.4

---
+++ project/trip/src/main/java/com/qtfycg/trip/TripApplication.java
@@ -0,0 +1,13 @@
+package com.qtfycg.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripApplication.class, args);
+    }
+
+}
---
+++ project/trip/src/test/java/com/qtfycg/trip/TripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qtfycg.trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
