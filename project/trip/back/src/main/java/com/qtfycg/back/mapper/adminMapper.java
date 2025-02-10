bd20b6021020fd3bd56548236f2c9aaf174e053e
qtfycg <1264318507@qq.com>
2025-02-10 19:17:50 +0800

    2.10.1

---
+++ project/trip/back/src/main/java/com/qtfycg/back/mapper/adminMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.admin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 12643
+* @description 针对表【admin】的数据库操作Mapper
+* @createDate 2025-02-10 17:59:39
+* @Entity generator.domain.admin
+*/
+public interface adminMapper extends BaseMapper<admin> {
+
+}
+
+
+
+
