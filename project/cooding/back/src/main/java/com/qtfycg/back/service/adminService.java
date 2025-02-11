c88439e58f7d9ca4510653a988c0363a02cc2e0a
qtfycg <1264318507@qq.com>
2025-02-11 18:31:47 +0800

    2.11.1

--- project/cooding/back/src/main/java/com/qtfycg/back/service/adminService.java
+++
@@ -1,9 +0,0 @@
-package com.qtfycg.back.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.qtfycg.back.domain.admin;
-
-
-public interface adminService<adminName> extends IService<admin> {
-
-}
