0f3ee59e2a364fa8315288098be262f7778f8cd1
qtfycg <1264318507@qq.com>
2025-07-02 12:14:21 +0800

    7.1

---
+++ project/back/CtripSystem/common/src/main/java/com/qtfycg/common/mq/FanoutMessageSender.java
@@ -0,0 +1,22 @@
+package com.qtfycg.common.mq;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FanoutMessageSender {
+    @Resource
+    RabbitTemplate rabbitTemplate;
+
+    public void send(String exchangeName, Object message) {
+        try {
+            rabbitTemplate.convertAndSend(exchangeName, "", message);
+            log.info("✅ 广播消息已发送至 [{}]：{}", exchangeName, message);
+        } catch (Exception e) {
+            log.error("❌ 广播消息发送失败", e);
+        }
+    }
+}
---
+++ project/back/CtripSystem/notify/src/main/java/com/qtfycg/notify/notifySpringApplication.java
@@ -0,0 +1,11 @@
+package com.qtfycg.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class notifySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(notifySpringApplication.class, args);
+    }
+}
