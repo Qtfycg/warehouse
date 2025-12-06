a878147f2553fa432ccb069ae1411c54e580c526
qtfycg <1264318507@qq.com>
2025-12-06 22:56:05 +0800

    12.6

---
+++ project/back/TripSystem/gateway/src/main/java/com/qtfycg/gateway/gatewaySpringApplication.java
@@ -0,0 +1,15 @@
+package com.qtfycg.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class gatewaySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(gatewaySpringApplication.class, args);
+        log.info("==================== 网关启动成功 ===================");
+        
+    }
+}
