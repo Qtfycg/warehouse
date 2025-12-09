bdfb6887f278b52338bbd43d1088e8a2cc1506f5
qtfycg <1264318507@qq.com>
2025-12-09 23:07:48 +0800

    12.6

--- project/front/LefitSystem/vite.config.js
+++ project/front/LefitSystem/vite.config.js
@@ -1,7 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  server:{
+    host:'127.0.0.1',
+    port:8080
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
